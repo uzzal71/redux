39f0ce037f0993dfa8d101fac622971191dc2c8f
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-18 16:07:27 +0600

    module 2:2.9 Redux Middleware concept

---
+++ module 2/2.9-Redux-Middleware-Connect/src/App.jsx
@@ -0,0 +1,24 @@
+import { Provider } from "react-redux";
+import HooksCounter from "./components/HooksCounter";
+import store from "./redux/store";
+import DynamicHooksCounter from './components/DynamicHooksCounter';
+import VariableCounter from './components/VariableCounter';
+
+export default function App() {
+    return (
+        <Provider store={store}>
+            <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+                <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+                    Simple Counter Application
+                </h1>
+
+                <div className="max-w-md mx-auto mt-10 space-y-5">
+                    <HooksCounter />
+                    <DynamicHooksCounter />
+                    <VariableCounter />
+                    <VariableCounter dynamic={true}/>
+                </div>
+            </div>
+        </Provider>
+    );
+}
