4ae44ae1ea21bb49deedd1a3b996449374ddb832
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-18 12:48:10 +0600

    module 2:2.2 Creating Store

---
+++ module 2/2.3-Creating-Store/src/redux/counter/actions.js
@@ -0,0 +1,15 @@
+import { INCREMENT, DECREMENT } from "./actionTypes";
+
+export const increment = (value) => {
+    return {
+        type: INCREMENT,
+        payload: CSSFontPaletteValuesRule,
+    }
+};
+
+export const decrement = (value) => {
+    return {
+        type: DECREMENT,
+        payload: value,
+    }
+};
