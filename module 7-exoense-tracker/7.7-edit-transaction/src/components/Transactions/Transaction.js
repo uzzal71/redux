a2a498fcae5a08aab504563dfddd36864864cec7
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-20 19:57:33 +0600

    module 7 - 7.7 edit transaction

---
+++ module 7-exoense-tracker/7.7-edit-transaction/src/components/Transactions/Transaction.js
@@ -0,0 +1,28 @@
+import { useDispatch } from "react-redux";
+import deleteImage from "../../assets/images/delete.svg";
+import editImage from "../../assets/images/edit.svg";
+import { editActive } from "../../features/transaction/transactionSlice";
+
+export default function Transaction({ transaction }) {
+  const { name, amount, type } = transaction || {};
+  const dispatch = useDispatch();
+
+  const handleEdit = () => {
+    dispatch(editActive(transaction));
+  };
+
+  return (
+    <li className={`transaction ${type}`}>
+      <p>{name}</p>
+      <div className="right">
+        <p>৳ {amount}</p>
+        <button className="link" onClick={handleEdit}>
+          <img alt="Edit" className="icon" src={editImage} />
+        </button>
+        <button className="link">
+          <img alt="Delete" className="icon" src={deleteImage} />
+        </button>
+      </div>
+    </li>
+  );
+}
