9a708ac79020db3ad76f27ce44415cceadc95af9
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-20 16:09:49 +0600

    module 6 - 6.9 Video Feature

---
+++ module 6/6.9-video-feature/src/components/tags/Tag.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tag = ({ title }) => {
+  return (
+    <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
+      { title }
+    </div>
+  );
+};
+
+export default Tag;
