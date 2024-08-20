63679d94ff047f7a48c9ec7fb150bae899316381
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-20 15:50:23 +0600

    module 6 - 6.8 Tags Feature 2

---
+++ module 6/6.8-tags-feature-2/src/components/tags/Tag.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Tag = ({ title }) => {
+  return (
+    <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
+      { title }
+    </div>
+  );
+};
+
+export default Tag;
+
+{/* <div class="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer">
+  redux
+</div>; */}
