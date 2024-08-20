6228e97cd9af561c5ea93bc39e897ba327cbd249
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-20 19:53:09 +0600

    module 7 - 7.5 list transaction

---
+++ module 7-exoense-tracker/7.6-list-transaction/src/components/Transactions/Transactions.js
@@ -0,0 +1,43 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import Transaction from "./Transaction";
+import { fetchTransactions } from "../../features/transaction/transactionSlice";
+
+export default function Transactions() {
+   const dispatch = useDispatch();
+
+    const { transactions, isLoading, isError } = useSelector(
+        (state) => state.transaction
+    );
+
+    useEffect(() => {
+      dispatch(fetchTransactions());
+    }, [dispatch]);
+
+    // decide what to render
+    let content = null;
+    if (isLoading) content = <p>Loading...</p>;
+
+    if (!isLoading && isError)
+        content = <p className="error">There was an error occured</p>;
+
+    if (!isLoading && !isError && transactions?.length > 0) {
+        content = transactions.map((transaction) => (
+            <Transaction key={transaction.id} transaction={transaction} />
+        ));
+    }
+
+    if (!isLoading && !isError && transactions?.length === 0) {
+        content = <p>No transactions found!</p>;
+    }
+
+    return (
+        <>
+            <p className="second_heading">Your Transactions:</p>
+
+            <div className="conatiner_of_list_of_transactions">
+                <ul>{content}</ul>
+            </div>
+        </>
+    );
+}
