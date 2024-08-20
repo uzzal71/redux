1718279c00b8cf005058a7ce6f70e085bb6ef313
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-20 16:30:52 +0600

    module 6 - 6.10 Related Video Feature

---
+++ module 6/6.10-related-video-feature/src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import relatedVideosReducer from "../features/relatedVideos/relatedVideosSlice";
+import tagsReducer from "../features/tags/tagsSlice";
+import videoReducer from "../features/video/videoSlice";
+import videosReducer from "../features/videos/videosSlice";
+
+export const store = configureStore({
+    reducer: {
+        videos: videosReducer,
+        tags: tagsReducer,
+        video: videoReducer,
+        relatedVideos: relatedVideosReducer,
+    },
+});
