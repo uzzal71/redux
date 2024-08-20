d1dd7773a26f7b1b2c82ef84cec052a474126c59
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-20 13:44:37 +0600

    module 6 - 6.4 setup routing - react-router-dom

---
+++ module 6/6.4-setup-routing-ract-router-dom/src/components/navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Search from "./Search";
+import logoImage from "../../assets/lws.svg";
+import SearchIcon from "../../assets/search.svg";
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-slate-100 shadow-md">
+      <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3">
+        <Link to="/">
+          <img className="h-10" src={logoImage} alt="Learn with Sumit" />
+        </Link>
+        <div className="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
+          <Search />
+          <img
+            className="inline h-4 cursor-pointer"
+            src={SearchIcon}
+            alt="Search"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
