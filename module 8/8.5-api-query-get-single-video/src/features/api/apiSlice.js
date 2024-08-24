270ebb33ced4b484cde7582991ce0d4f4fc0a9d1
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-24 17:29:11 +0600

    8.5 - Api Query - Get related Videos

---
+++ module 8/8.5-api-query-get-single-video/src/features/api/apiSlice.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:9000",
+  }),
+  endpoints: (builder) => ({
+    getVideos: builder.query({
+      query: () => "/videos",
+    }),
+    getVideo: builder.query({
+      query: (videoId) => `/videos/${videoId}`,
+    }),
+    getRelatedVideos: builder.query({
+      query: ({ id, title }) => {
+        const tags = title.split(" ");
+        const likes = tags.map((tag) => `title_like=${tag}`);
+        const queryString = `/videos?${likes.join("&")}&_limit=4`;
+        return queryString;
+      },
+    }),
+  }),
+});
+
+export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery } =
+  apiSlice;
