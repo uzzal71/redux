f2c8923acf7e1cd7914e3c905be27771825b168a
uzzal71 <uzzalroy.acm@gmail.com>
2024-08-25 13:20:59 +0600

    8.9 - Cache Behavior - revalidation

---
+++ module 8/8.9-cache-behavior-revalidation/src/features/api/apiSlice.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:9000",
+  }),
+  tagTypes: ["Videos"],
+  endpoints: (builder) => ({
+    getVideos: builder.query({
+      query: () => "/videos",
+      keepUnusedDataFor: 600,
+      providesTags: ["Videos"],
+    }),
+    getVideo: builder.query({
+      query: (videoId) => `/videos/${videoId}`,
+    }),
+    getRelatedVideos: builder.query({
+      query: ({ id, title }) => {
+        const tags = title.split(" ");
+        const likes = tags.map((tag) => `title_like=${tag}`);
+        const queryString = `/videos?${likes.join("&")}&_limit=4`;
+        return queryString;
+      },
+    }),
+    addVideo: builder.mutation({
+      query: (data) => ({
+        url: "/videos",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Videos"],
+    }),
+  }),
+});
+
+export const {
+  useGetVideosQuery,
+  useGetVideoQuery,
+  useGetRelatedVideosQuery,
+  useAddVideoMutation,
+} = apiSlice;
